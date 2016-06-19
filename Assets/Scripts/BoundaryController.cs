624dc31800d67adce67218daa34218a6ddeeafc4
Claudio Pupparo <claudio.pupparo@gmail.com>
2016-06-19 15:59:06 +0200

    Bolts are destroyed when they leave the world boundaries

---
+++ Assets/Scripts/BoundaryController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoundaryController : MonoBehaviour
+{
+    public GameObject[] destroyableObjects;
+
+    // Delete destroyable object when they leave the boundaries
+    void OnTriggerExit(Collider other)
+    {
+        foreach (GameObject destroyableObj in destroyableObjects)
+        {
+            if (other.gameObject.CompareTag(destroyableObj.tag))
+            {
+                Destroy(other.gameObject);
+                break;
+            }
+        }
+    }
+}
